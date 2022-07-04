9a685d1f18a66f9e94f968b39580c812a15f551c
Alejo Almela <74593956+Sonaho@users.noreply.github.com>
2022-07-04 11:58:30 +0200

    Update sitenavigation.py

--- features/steps/sitenavigation.py
+++ features/steps/sitenavigation.py
@@ -19,6 +19,7 @@ def step_impl(context, web, title):
 @when ("I search selenium")
 def step_impl(context):
 	context.driver.find_element(By.ID, "search").send_keys('selenium')
+	çcontext.driver.find_element(By.CLASS_NAME, "search-form__button").click()
 
 
 @then ('I landing at page "{web}"')
